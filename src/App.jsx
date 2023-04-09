0c7e733ea5cab3bb3774cedc8ef8d8b3e2532fd1
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-10 00:09:20 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h2 className='text-5xl font-semibold'>Welcome to Iconic-Job</h2>
+    </div>
+  );
+};
+
+export default App;
