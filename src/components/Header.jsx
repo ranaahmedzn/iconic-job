b621fcb3255e2806ebd5aec8a54e7bc5a59fc9d7
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-10 10:40:50 +0600

    added responsive design for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className='w-full bg-gray-100'>
-            <div className='container py-12 mx-auto flex justify-between items-center'>
-                <a className='font-bold text-3xl ' href="/">Iconic-Job</a>
+            <div className='hidden container py-12 mx-auto lg:flex justify-between items-center'>
+                <Link className='font-bold text-3xl ' to="/">Iconic-Job</Link>
                 <ul className='flex gap-8'>
                     <li className='default'><NavLink to="/" className={({ isActive }) =>
                       isActive
@@ -29,6 +32,45 @@ const Header = () => {
                     }>Blog</NavLink></li>
                 </ul>
                 <button className='btn-primary'>Start Applying</button>
+            </div>
+
+            <div className='lg:hidden relative h-[75px] px-3 flex justify-between items-center'>
+                <Link className='font-bold text-3xl ' to="/">Iconic-Job</Link>
+                
+                <div onClick={() => setIsOpen(!isOpen)}>
+                    {
+                        isOpen 
+                        ? <XMarkIcon className="h-8 w-8 text-black" />
+                        : <Bars3Icon className="h-8 w-8 text-black" />
+                    }
+                </div>
+
+                <ul className={`flex flex-col absolute gap-8 py-8 px-6 ${isOpen ? 'top-[75px] left-0' : "top-[75px] -left-[1000px]"} duration-500 bg-gray-700 w-full`}>
+                    <li className='default2'><NavLink to="/" className={({ isActive }) =>
+                      isActive
+                        ? "active"
+                        : ""
+                    }>Home</NavLink></li>
+                    <li className='default2'><NavLink to="/statistics" className={({ isActive }) =>
+                      isActive
+                        ? "active"
+                        : ""
+                    }>Statistics</NavLink></li>
+                    <li className='default2'><NavLink to="/applied-jobs" className={({ isActive }) =>
+                      isActive
+                        ? "active"
+                        : ""
+                    }>Applied Jobs</NavLink></li>
+                    <li className='default2'><NavLink to="/blog" className={({ isActive }) =>
+                      isActive
+                        ? "active"
+                        : ""
+                    }>Blog</NavLink></li>
+                    <li>
+                        <button className='btn-primary'>Start Applying</button>
+                    </li>
+                </ul>
+
             </div>
         </div>
     );
